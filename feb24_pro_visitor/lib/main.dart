5671f91484520eacbe887d17c438555ae98c098c
Paul <paul@iconica.nl>
2024-02-29 14:17:23 +0100

    feat: add the splash screen

--- feb24_pro_visitor/lib/main.dart
+++ feb24_pro_visitor/lib/main.dart
@@ -1,20 +1,19 @@
+import 'package:feb24_pro_visitor/ui/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp( MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Your App Title',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: SplashScreen(), 
     );
   }
 }

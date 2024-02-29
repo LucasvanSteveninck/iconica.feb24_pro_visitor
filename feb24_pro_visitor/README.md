aaeb6c763d9de63513eabb7b423d34b049f8e625
Paul <paul@iconica.nl>
2024-02-29 13:36:41 +0100

    feat: add empty project

---
+++ feb24_pro_visitor/README.md
@@ -0,0 +1,3 @@
+# feb24_pro_visitor
+
+A new Flutter project.
---
+++ feb24_pro_visitor/android/app/src/main/kotlin/com/example/feb24_pro_visitor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.feb24_pro_visitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
